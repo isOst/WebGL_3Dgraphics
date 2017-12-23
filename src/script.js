38c149dd340e12fdbaa9e2be8c906863672e6d85
Igor Ostapov <ostapovi@digicode.net>
2017-12-23 18:03:40 +0200

    animate multiply vertices

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 let gl,
     vertices,
-    shaderProgram;
+    shaderProgram,
+    vertexCount = 5000;
 
 initGL = () => {
     let canvas = document.getElementById("canvas");
@@ -66,34 +67,41 @@ createShaders = () => {
  * set vars of shader's vertexes through attrs
  */
 createVertices = () => {
-    vertices = [
-        -0.9, -0.9, 0.0,
-         0.0,  0.9, 0.0,
-         0.9, -0.9, 0.0,
-    ];
+    vertices = [];
+    for(let i = 0; i < vertexCount; i++) {
+        vertices.push(Math.random() * 2 - 1);
+        vertices.push(Math.random() * 2 - 1);
+    }
     /**
      * create buffer array with vertices coords
      */
     let buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
 
     let coords = gl.getAttribLocation(shaderProgram, "coords");
     // gl.vertexAttrib3f(coords, 0, 0, 0); change single point to poits array
-    gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(coords, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(coords);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    //gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     let pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
-    gl.vertexAttrib1f(pointSize, 50);
+    gl.vertexAttrib1f(pointSize, 1);
 
     let color = gl.getUniformLocation(shaderProgram, "color");
     gl.uniform4f(color, 0, 0, 0, 1);
 }
 
 draw = () => {
+    for(let i = 0; i < vertexCount * 2; i += 2) {
+        vertices[i] += Math.random() * 0.01 - 0.005;
+        vertices[i + 1] += Math.random() * 0.01 - 0.005;
+    }
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(vertices));
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    gl.drawArrays(gl.POINTS, 0, vertexCount);
+
+    requestAnimationFrame(draw);
 };
 
 /**
